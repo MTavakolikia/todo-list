2cca003513979cd5382bebb0a9ad8327aca3b865
mohammadtk1994 <mohammadtavakolikia66@gmail.com>
2023-06-24 16:02:15 +0330

    some element and styles added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./assets/styles/styles.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1>Todo App</h1>
+        <form action="">
+          <input type="text" />
+          <button>Add</button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default App;
